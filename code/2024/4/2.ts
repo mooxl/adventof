53a3e3525771d8ef09349cc78f32a408c00ed2aa
Max Schmidt <max.schmidt@outlook.de>
2024-12-06 13:55:01 +0100

    code day 4 part 2
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

---
+++ code/2024/4/2.ts
@@ -0,0 +1,41 @@
+import { getInput } from "../../../utils.ts";
+
+const input = await getInput(import.meta.dirname);
+
+const lines: string[][] = input.split("\n").map((line) => line.split(""));
+
+let count = 0;
+
+for (const [indexLine, currentLine] of lines.entries()) {
+  for (const [indexLetter, currentLetter] of currentLine.entries()) {
+    if (currentLetter !== "A") continue;
+    if (
+      !lines[indexLine - 1] || !lines[indexLine + 1] ||
+      !lines[indexLine - 1][indexLetter - 1] ||
+      !lines[indexLine - 1][indexLetter + 1] ||
+      !lines[indexLine + 1][indexLetter - 1] ||
+      !lines[indexLine + 1][indexLetter + 1]
+    ) {
+      continue;
+    }
+    const x = [
+      lines[indexLine - 1][indexLetter - 1],
+      lines[indexLine - 1][indexLetter + 1],
+      lines[indexLine + 1][indexLetter + 1],
+      lines[indexLine + 1][indexLetter - 1],
+    ];
+
+    if (
+      x.filter((char) => char === "M").length !== 2 ||
+      x.filter((char) => char === "S").length !== 2
+    ) {
+      continue;
+    }
+
+    if (x[0] !== x[2]) {
+      count++;
+    }
+  }
+}
+
+console.log(count);
--- utils.ts
+++ utils.ts
@@ -1,7 +1,7 @@
-export const getInput = async (path?: string) => {
+export const getInput = async (path: string | undefined, sample?: string) => {
   if (!path) {
     throw new Error();
   }
 
-  return await Deno.readTextFile(`${path}/input.txt`);
+  return await Deno.readTextFile(`${path}/${sample ?? "input"}.txt`);
 };
